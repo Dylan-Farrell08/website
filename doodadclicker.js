013531ed22db126b5e77f4ece11e05e5bd70d6f7
Dylan-Farrell08 <dylan.farrell.968@accesscreative.ac.uk>
2025-06-25 14:38:26 +0000

    FUCK INFINITE BUILDINGs

--- doodadclicker.js
+++ doodadclicker.js
@@ -7,35 +7,35 @@ function clickBtn() {
     document.getElementById("doodadCounter").innerHTML = "amount collected = " + doodad;
 }
 function buybuilding() {
-    if(doodad > 100 * 1.2 ^ (building)||doodad == 100 * 1.2 ^ (building)){
+    if(doodad > (100 * 1.2 ^ (building))||doodad == (100 * 1.2 ^ (building))){
         doodad -= 100 * 1.2 ^ (building);
         building += 1;
         document.getElementById("doodadCounter").innerHTML = "amount collected = " + doodad;
-        document.getElementById("buildingCounter").innerHTML = "building collected = " + building;
-        document.getElementById("buildingbutton").innerHTML = "doodadbuilding price = " + 100 * 1.2 ^ (building);
+        document.getElementById("buildingbutton").innerHTML = "buildings collected = " + building;
+        document.getElementById("buildingCounter").innerHTML = "doodadbuilding price = " + ((100 * 1.2) ^ (building));
     }
 }
 function buyclickpower() {
-    if(doodad > (50 * 1.1 ^ (clickpower))||doodad == (50 * 1.1 ^ (clickpower))){
-        doodad -= (50 * 1.1 ^ (clickpower));
+    if(doodad > (50 * (1.1 ^ clickpower))||doodad == (50 * (1.1 ^ clickpower))){
+        doodad -= (50 * (1.1 ^ clickpower));
         clickpower += 1;
         document.getElementById("doodadCounter").innerHTML = "amount collected = " + doodad;
         document.getElementById("clickpowerCounter").innerHTML = "clickpower collected = " + clickpower;
-        document.getElementById("clickbutton").innerHTML = "clickstrength price = " + (50 * 1.1 ^ (clickpower));
+        document.getElementById("clickbutton").innerHTML = "clickstrength price = " + (50 * (1.1 ^ clickpower));
     }
 }
 function buypirates() {
-    if(doodad > (400 * 1.1 ^ (pirates))||doodad == (400 * 1.1 ^ (pirates))){
-        doodad -= (400 * 1.1 ^ (pirates));
+    if(doodad > ((400 * 1.1) ^ (pirates))||doodad == ((400 * 1.1) ^ (pirates))){
+        doodad -= ((400 * 1.1) ^ (pirates));
         pirates += 1;
-        document.getElementById("doodadCounter").innerHTML = "fleet size = " + doodad;
+        document.getElementById("doodadCounter").innerHTML = "amount collected = " + doodad;
         document.getElementById("pirateCounter").innerHTML = "pirates sailing = " + pirates;
-        document.getElementById("piratesbutton").innerHTML = "ship price = " + (400 * 1.1 ^ (pirates));
+        document.getElementById("piratesbutton").innerHTML = "ship price = " + ((400 * 1.1) ^ (pirates));
     }
 }
 setInterval(() => {
 doodad += building
 doodad += pirates * 6
 document.getElementById("doodadCounter").innerHTML = "amount collected = " + doodad;
-document.getElementById("buildingCounter").innerHTML = "building collected = " + building;
+document.getElementById("buildingCounter").innerHTML = "buildings collected = " + building;
 }, 1000);
